9c402348480853e700b859e8961511c606b20ab6
Christopher Peers <christopher_peers@outlook.com>
2018-02-19 09:45:59 +0000

    added a dot before env so they would be added to the process object

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,31 @@ import React from 'react';
 class Navbar extends React.Component {
 
     state = {
-        topics : []
+        topics: [],
+        topicsLoaded: false
+    }
+
+    fetchTopics = () => {
+        return fetch(`${process.env.REACT_APP_API_URL}/topics`)
+            .then(buffer => buffer.json())
+            .then(topics => topics)
+            .catch(err => console.log(err));
+    }
+
+    componentWillMount() {
+        this.fetchTopics()
+            .then(res => this.setState({
+                topics: res.topics,
+                topicsLoaded: true
+            }))
     }
 
     render() {
+        
         return (
             <h1>Navbar Goes Here</h1>
         )
-    } 
+    }
 
 }
 
