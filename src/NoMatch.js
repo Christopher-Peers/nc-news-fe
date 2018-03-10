9761a1035c616aeeb31cd7d21d18c46b8e0d1225
Christopher Peers <christopher_peers@outlook.com>
2018-03-10 12:35:48 +0000

    refactored 404 page to include the non-existant path

--- src/NoMatch.js
+++ src/NoMatch.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const NoMatch = () => {
-    return (
-        <h1>404 Not Found</h1>
-    )
+const NoMatch = (props) => {
+  
+  return (
+    <div className="mt-3">
+      <h1>404 Not Found</h1>
+      <p>The path "{props.location.pathname}" you have requested does not exist.</p>
+    </div>
+  )
 }
 
 export default NoMatch;
